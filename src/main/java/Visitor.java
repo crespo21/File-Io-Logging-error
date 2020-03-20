248d63514930d36483c7bd9db153359a5685ca43
crespo21 <59964121+crespo21@users.noreply.github.com>
2020-03-20 08:31:58 +0200

    File-io-logger-error initial commit

--- src/main/java/Visitor.java
+++ src/main/java/Visitor.java
@@ -17,86 +17,80 @@ public class Visitor {
 
 
     private static final Logger logger = LogManager.getLogger(Visitor.class.getName());
-
     public static boolean saves(String fullName, int Age, String comments, String Assistant) throws IOException {
-
         FileWriter fileWriter = null;
         boolean checker = false;
-        try {
-
+         try {
             if (!fullName.isEmpty()) {
                 checker = true;
                 File visitorFile = new File("visitor_ " +
                         fullName.replace(" ", "_")+".txt");
                 if (visitorFile.createNewFile()) {
                      fileWriter = new FileWriter(visitorFile);
-
                      fileWriter.write("Full name: " + fullName + "\n" +
-                       "Age: " + Age + "\n" +
+                       "Age: " + Age+ " years old"+ "\n" +
                        "Date of visit: " + LocalDate.now() + "\n" +
                        "Time of visit: " + LocalTime.now() + "\n" + "Comments: " + comments + "\n" +
                        "Assistant: " + Assistant);
-                     logger.info("Recruit's file successfully created!");
-                } else{
+                    System.out.println("Recruit's file successfully created!");
+                }
+                else{
                      logger.error("file already exist");
-                 }
+                }
 
-            } else{
-                checker = false;
-                logger.error("full name cannot be empty,");
-                logger.error("File not  created :(");
             }
-        } catch (IOException e) {
-            logger.error(saves(fullName,Age,comments,Assistant));
-            e.printStackTrace();
-        }
-
-        fileWriter.close();
+            else{
+                checker = false;
+                logger.error("File not  created! because full name cannot be empty!");
+            }
+         }
+         catch (IOException e) {
+                logger.error(saves(fullName,Age,comments,Assistant));
+                e.printStackTrace();
+         }
+         fileWriter.close();
 
         return checker;
 
     }
-    public static boolean load(String fullName){
+    public static String load(String fullName){
         try{
             File visitorFile = new File("visitor_ " +
                     fullName.replace(" ", "_")+".txt");
-            Scanner myReader = new Scanner(visitorFile);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
+            Scanner reader = new Scanner(visitorFile);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
                 System.out.println(data);
             }
-            myReader.close();
+            reader.close();
         } catch (FileNotFoundException e) {
             logger.error("File not Found :(");
             e.getStackTrace();
 
         }
-        return true;
+        return fullName;
     }
         public static void main(String[] args)  {
         try {
-            System.out.println("Welcome to umuzi");
+            System.out.println("Welcome to Umuzi's VisInfoCap");
+            System.out.println(" ");
+
             Scanner input = new Scanner(System.in);
-            System.out.println("please enter your name");
-
+            System.out.println("please enter your name: ");
             fullName = input.nextLine();
-
-            System.out.println("Full Name: " + fullName);
             Scanner input1 = new Scanner(System.in);
             System.out.println("please enter your age");
-
             Age = input1.nextInt();
-
-            System.out.println("Age: " + Age);
             Scanner input2 = new Scanner(System.in);
 
             System.out.println("please enter your comments");
             comments = input2.nextLine();
-            System.out.println("Comments: " + comments);
-
-            System.out.println();
+            System.out.println(" ");
             logger.error(saves(fullName, Age, comments, Assistant));
-            System.out.println(load(fullName));
+            Visitor vst = new Visitor();
+            vst.load(fullName);
+            System.out.println(" ");
+            System.out.println(("This file belong's to " + fullName));
         } catch (NullPointerException | IOException e){
             e.getMessage();
 
